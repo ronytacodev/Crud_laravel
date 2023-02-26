358e4c608e4cba924bd9ba49da219d34e8212f99
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-02-26 09:40:29 -0500

    lets create a form for receive data

--- resources/views/empleado/create.blade.php
+++ resources/views/empleado/create.blade.php
@@ -1,5 +1,28 @@
 Formulario de creación de empleados
 
-<form action="" method="post">
-    {{-- me quede en el min 38:!5 --}}
+<form action="" method="post" enctype="multipart/form-data">
+    
+    <label for="Nombre"> Nombre </label>
+    <input type="text" name="Nombre" id="Nombre">
+    <br>
+
+    <label for="ApellidoPaterno"> ApellidoPaterno </label>
+    <input type="text" name="ApellidoPaterno" id="ApellidoPaterno">
+    <br>
+
+    <label for="ApellidoMaterno"> ApellidoMaterno </label>
+    <input type="text" name="ApellidoMaterno" id="ApellidoMaterno">
+    <br>
+
+    <label for="Correo"> Correo </label>
+    <input type="text" name="Correo" id="Correo">
+    <br>
+
+    <label for="Foto"> Foto </label>
+    <input type="file" name="Foto" id="Foto">
+    <br>
+
+    <input type="submit" name="Enviar" id="Enviar">
+    <br>
+   
 </form>
