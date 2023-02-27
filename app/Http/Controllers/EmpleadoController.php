c490b1b753b9425126d26f6b98d10b61d8a28ec0
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-02-27 08:59:05 -0500

    lets create table with bootstrap 4 and retrieve records of database

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -13,7 +13,8 @@ class EmpleadoController extends Controller
     public function index()
     {
         //
-        return view('empleado.index');
+        $datos['empleados'] = Empleado::paginate(5);
+        return view('empleado.index', $datos);
     }
 
     /**
--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -1,1 +1,29 @@
 Mostrar la lista de empleados
+
+<table class="table table-light">
+    <thead class="thead-light">
+        <tr>
+            <th>#</th>
+            <th>Foto</th>
+            <th>Nombre</th>
+            <th>Apellido Paterno</th>
+            <th>Apellido Materno</th>
+            <th>Correo</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        @foreach ($empleados as $empleado)
+        <tr>
+            <td>{{ $empleado->id }}</td>
+            <td>{{ $empleado->Foto}}</td>
+            <td>{{ $empleado->Nombre}}</td>
+            <td>{{ $empleado->ApellidoPaterno}}</td>
+            <td>{{ $empleado->ApellidoMaterno}}</td>
+            <td>{{ $empleado->Correo}}</td>
+            <td>Editar | Borrar </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
