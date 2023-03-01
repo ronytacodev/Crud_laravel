314f5aa9a8fbc59263a5bf39615220e306b22f88
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-03-01 14:11:56 -0500

    add links

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -1,24 +1,29 @@
 <label for="Nombre"> Nombre </label>
-<input type="text" name="Nombre" value="{{ $empleado->Nombre}}" id="Nombre">
+<input type="text" name="Nombre" value="{{ isset($empleado->Nombre)?$empleado->Nombre:''}}" id="Nombre">
 <br>
 
 <label for="ApellidoPaterno"> ApellidoPaterno </label>
-<input type="text" name="ApellidoPaterno" value="{{ $empleado->ApellidoPaterno}}" id="ApellidoPaterno">
+<input type="text" name="ApellidoPaterno" value="{{ isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:''}}" id="ApellidoPaterno">
 <br>
 
 <label for="ApellidoMaterno"> ApellidoMaterno </label>
-<input type="text" name="ApellidoMaterno" value="{{ $empleado->ApellidoMaterno}}" id="ApellidoMaterno">
+<input type="text" name="ApellidoMaterno" value="{{ isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno:''}}" id="ApellidoMaterno">
 <br>
 
 <label for="Correo"> Correo </label>
-<input type="text" name="Correo" value="{{ $empleado->Correo}}" id="Correo">
+<input type="text" name="Correo" value="{{ isset($empleado->Correo)?$empleado->Correo:''}}" id="Correo">
 <br>
 
 <label for="Foto"> Foto </label>
-<img src="{{ asset('storage').'/'.$empleado->Foto}}" width="100" alt="">
-
+@if(isset($empleado->Foto))
+    <img src="{{ asset('storage').'/'.$empleado->Foto}}" width="100" alt="">
+@endif
 <input type="file" name="Foto" value="" id="Foto">
 <br>
 
 <input type="submit" value="Guardar datos">
+
+<a href="{{url('empleado/')}}"> Regresar </a>
+
 <br>
+
--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -1,4 +1,5 @@
 Mostrar la lista de empleados
+<a href="{{url('empleado/create')}}"> Registrar nuevo empleado</a>
 
 <table class="table table-light">
     <thead class="thead-light">
