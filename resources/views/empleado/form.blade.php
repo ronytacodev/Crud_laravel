4fc1e54fb03beca383da1d5a23d190454724515c
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-03-01 16:19:10 -0500

    apply styles to form buttons

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -1,16 +1,23 @@
 
 <h1> {{ $modo }} empleado </h1>
-<label for="Nombre"> Nombre </label>
-<input type="text" name="Nombre" value="{{ isset($empleado->Nombre)?$empleado->Nombre:''}}" id="Nombre">
-<br>
 
-<label for="ApellidoPaterno"> ApellidoPaterno </label>
-<input type="text" name="ApellidoPaterno" value="{{ isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:''}}" id="ApellidoPaterno">
-<br>
+<div class="form-group">
+    <label for="Nombre"> Nombre </label>
+    <input type="text" class="form-control" name="Nombre" value="{{ isset($empleado->Nombre)?$empleado->Nombre:''}}" id="Nombre">
+    <br>
+</div>
 
-<label for="ApellidoMaterno"> ApellidoMaterno </label>
-<input type="text" name="ApellidoMaterno" value="{{ isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno:''}}" id="ApellidoMaterno">
-<br>
+<div class="form-group">
+    <label for="ApellidoPaterno"> ApellidoPaterno </label>
+    <input type="text" class="form-control" name="ApellidoPaterno" value="{{ isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:''}}" id="ApellidoPaterno">
+    <br>
+</div>
+
+<div class="form-group">
+    <label for="ApellidoMaterno"> ApellidoMaterno </label>
+    <input type="text" class="form-control" name="ApellidoMaterno" value="{{ isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno:''}}" id="ApellidoMaterno">
+    <br>
+</div>
 
 <label for="Correo"> Correo </label>
 <input type="text" name="Correo" value="{{ isset($empleado->Correo)?$empleado->Correo:''}}" id="Correo">
@@ -29,3 +36,4 @@
 
 <br>
 
+{{-- me quede en el min 2:11:35 --}}
