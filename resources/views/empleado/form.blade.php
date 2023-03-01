96242e75e52708a7fffdb0f7fc124d13a6a6f71f
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-03-01 18:56:50 -0500

    adjust styles of form

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -4,36 +4,35 @@
 <div class="form-group">
     <label for="Nombre"> Nombre </label>
     <input type="text" class="form-control" name="Nombre" value="{{ isset($empleado->Nombre)?$empleado->Nombre:''}}" id="Nombre">
-    <br>
 </div>
 
 <div class="form-group">
     <label for="ApellidoPaterno"> ApellidoPaterno </label>
     <input type="text" class="form-control" name="ApellidoPaterno" value="{{ isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:''}}" id="ApellidoPaterno">
-    <br>
 </div>
 
 <div class="form-group">
     <label for="ApellidoMaterno"> ApellidoMaterno </label>
     <input type="text" class="form-control" name="ApellidoMaterno" value="{{ isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno:''}}" id="ApellidoMaterno">
+</div>
+
+<div class="form-group">
+    <label for="Correo"> Correo </label>
+    <input type="text" class="form-control" name="Correo" value="{{ isset($empleado->Correo)?$empleado->Correo:''}}" id="Correo">
     <br>
 </div>
 
-<label for="Correo"> Correo </label>
-<input type="text" name="Correo" value="{{ isset($empleado->Correo)?$empleado->Correo:''}}" id="Correo">
-<br>
+<div class="form-group">
+    <label for="Foto"> Foto: </label>
+        @if(isset($empleado->Foto))
+            <img class="img-thumbnail img-fluid" src="{{ asset('storage').'/'.$empleado->Foto}}" width="100" alt="">
+        @endif
+    <input type="file" class="form-control" name="Foto" value="" id="Foto">
+    <br>
+</div>
 
-<label for="Foto"> Foto </label>
-@if(isset($empleado->Foto))
-    <img src="{{ asset('storage').'/'.$empleado->Foto}}" width="100" alt="">
-@endif
-<input type="file" name="Foto" value="" id="Foto">
-<br>
+<input class="btn btn-success" type="submit" value="{{$modo}} datos">
 
-<input type="submit" value="{{$modo}} datos">
-
-<a href="{{url('empleado/')}}"> Regresar </a>
+<a class="btn btn-primary" href="{{url('empleado/')}}"> Regresar </a>
 
 <br>
-
-{{-- me quede en el min 2:11:35 --}}
--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,11 @@ Route::get('/home', [EmpleadoController::class, 'index'])->name('home');
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/', [EmpleadoController::class, 'index'])->name('home');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
