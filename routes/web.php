321b0b48b9021d6fb36218c9697b7f8045720aef
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-03-01 16:04:43 -0500

    aplicate segurity options

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,10 @@ Route::get('/', function () {
 
 // Route::get('empleado/create', [EmpleadoController::class, 'create']);
 
-Route::resource('empleado', EmpleadoController::class);
+Route::resource('empleado', EmpleadoController::class)->middleware('auth');
 
-Auth::routes();
+// Auth::routes();
+Auth::routes(['register' => false, 'reset' => false]);
 
 Route::get('/home', [EmpleadoController::class, 'index'])->name('home');
 
